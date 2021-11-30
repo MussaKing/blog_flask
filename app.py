a1eb1d195d272a4457784648dce16736fdfc1fdb
MussaKing <mussa.overlord@gmail.com>
2021-11-30 20:10:16 +0300

    Lesson 1

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/hi')
+def hi_world():  # put application's code here
+    return 'hi World!'
+
+
 if __name__ == '__main__':
     app.run()
