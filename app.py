3f33041a6cbbca6936c462597e1bb1be5c7bf50a
MussaKing <mussa.overlord@gmail.com>
2021-11-30 23:38:13 +0300

    Lesson 2

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
 
 
-@app.route('/hi')
-def hi_world():  # put application's code here
-    return 'hi World!'
+@app.route('/user/<string:name>/<int:id>')
+def print_user(name,id):
+    name = name.title() # Заглавная буква
+    return f'User {name}, id {str(id)}'
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
